e0b4caf7af1a512cf21d7cd81c2b60c7b464722f
portalmoesd <portalmoesd@gmail.com>
2026-03-07 13:36:08 +0400

    Add files via upload

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -79,7 +79,7 @@
 
     function iconSvg(name){
       const svgs = {
-        logo: `<?xml version="1.0" encoding="utf-8"?> <!-- Generator: Adobe Illustrator 24.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0) --> <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 350 350" style="enable-background:new 0 0 350 350;" xml:space="preserve"> <g> <g> <g> <g> <path d="M266.5,254.4c2.9,0,5.5,1.1,7.5,2.8v-14.1c0-4.2-3.4-7.5-7.5-7.5h0c-4.2,0-7.5,3.4-7.5,7.5v14.1 C261,255.5,263.6,254.4,266.5,254.4z"/> <path d="M266.5,277.6c-2.9,0-5.5-1.1-7.5-2.8V289c0,4.2,3.4,7.5,7.5,7.5h0c4.2,0,7.5-3.4,7.5-7.5v-14.1 C272,276.6,269.4,277.6,266.5,277.6z"/> </g> <g> <path d="M289.5,258.5h-14.2c1.8,2,2.8,4.7,2.8,7.6c0,2.9-1,5.5-2.7,7.5h14.1c4.2,0,7.5-3.4,7.5-7.5l0,0 C297,261.8,293.6,258.5,289.5,258.5z"/> <path d="M255,266.1c0-2.9,1.1-5.6,2.8-7.6h-14.2c-4.2,0-7.5,3.4-7.5,7.5l0,0c0,4.2,3.4,7.5,7.5,7.5h14.1 C256,271.5,255,268.9,255,266.1z"/> </g> </g> <g> <g> <path d="M84.8,254.4c2.9,0,5.5,1.1,7.5,2.8V243c0-4.2-3.4-7.5-7.5-7.5h0c-4.2,0-7.5,3.4-7.5,7.5v14.1 C79.3,255.5,82,254.4,84.8,254.4z"/> <path d="M84.8,277.6c-2.9,0-5.5-1.1-7.5-2.8V289c0,4.2,3.4,7.5,7.5,7.5h0c4.2,0,7.5-3.4,7.5-7.5v-14.1 C90.4,276.6,87.7,277.6,84.8,277.6z"/> </g> <g> <path d="M107.8,258.5H93.6c1.8,2,2.8,4.7,2.8,7.6c0,2.9-1,5.5-2.7,7.5h14.1c4.2,0,7.5-3.4,7.5-7.5l0,0 C115.3,261.8,112,258.5,107.8,258.5z"/> <path d="M73.3,266.1c0-2.9,1.1-5.6,2.8-7.6H61.9c-4.2,0-7.5,3.4-7.5,7.5l0,0c0,4.2,3.4,7.5,7.5,7.5H76 C74.3,271.5,73.3,268.9,73.3,266.1z"/> </g> </g> </g> <g> <g> <path d="M266.5,72.7c2.9,0,5.5,1.1,7.5,2.8V61.4c0-4.2-3.4-7.5-7.5-7.5h0c-4.2,0-7.5,3.4-7.5,7.5v14.1 C261,73.8,263.6,72.7,266.5,72.7z"/> <path d="M266.5,95.9c-2.9,0-5.5-1.1-7.5-2.8v14.1c0,4.2,3.4,7.5,7.5,7.5h0c4.2,0,7.5-3.4,7.5-7.5V93.1 C272,94.9,269.4,95.9,266.5,95.9z"/> </g> <g> <path d="M289.5,76.8h-14.2c1.8,2,2.8,4.7,2.8,7.6c0,2.9-1,5.5-2.7,7.5h14.1c4.2,0,7.5-3.4,7.5-7.5v0 C297,80.2,293.6,76.8,289.5,76.8z"/> <path d="M255,84.4c0-2.9,1.1-5.6,2.8-7.6h-14.2c-4.2,0-7.5,3.4-7.5,7.5v0c0,4.2,3.4,7.5,7.5,7.5h14.1C256,89.9,255,87.2,255,84.4 z"/> </g> </g> <g> <g> <path d="M84.8,72.7c2.9,0,5.5,1.1,7.5,2.8V61.4c0-4.2-3.4-7.5-7.5-7.5h0c-4.2,0-7.5,3.4-7.5,7.5v14.1 C79.3,73.8,82,72.7,84.8,72.7z"/> <path d="M84.8,95.9c-2.9,0-5.5-1.1-7.5-2.8v14.1c0,4.2,3.4,7.5,7.5,7.5h0c4.2,0,7.5-3.4,7.5-7.5V93.1 C90.4,94.9,87.7,95.9,84.8,95.9z"/> </g> <g> <path d="M107.8,76.8H93.6c1.8,2,2.8,4.7,2.8,7.6c0,2.9-1,5.5-2.7,7.5h14.1c4.2,0,7.5-3.4,7.5-7.5v0 C115.3,80.2,112,76.8,107.8,76.8z"/> <path d="M73.3,84.4c0-2.9,1.1-5.6,2.8-7.6H61.9c-4.2,0-7.5,3.4-7.5,7.5v0c0,4.2,3.4,7.5,7.5,7.5H76 C74.3,89.8,73.3,87.2,73.3,84.4z"/> </g> </g> </g> <g> <g> <path d="M200.6,149.4L200.6,149.4c3.6,3.6,3.6,9.5,0,13.1l-38.1,38.1c-3.6,3.6-9.5,3.6-13.1,0l0,0c-3.6-3.6-3.6-9.5,0-13.1 l38.1-38.1C191.1,145.8,197,145.8,200.6,149.4z"/> <path d="M194,146.7L194,146.7c5.1,0,9.2,4.1,9.2,9.2l0,32.7c0,5.1-4.1,9.2-9.2,9.2h0c-5.1,0-9.2-4.1-9.2-9.2l0-32.7 C184.8,150.8,188.9,146.7,194,146.7z"/> <path d="M203.3,155.9L203.3,155.9c0,5.1-4.1,9.2-9.2,9.2l-32.7,0c-5.1,0-9.2-4.1-9.2-9.2v0c0-5.1,4.1-9.2,9.2-9.2l32.7,0 C199.2,146.7,203.3,150.8,203.3,155.9z"/> </g> <g> <g> <path d="M175,117.3c14.3,0,27.4,5.2,37.5,13.9V60.9c0-20.7-16.8-37.5-37.5-37.5h0c-20.7,0-37.5,16.8-37.5,37.5v70.3 C147.6,122.6,160.7,117.3,175,117.3z"/> <path d="M175,232.7c-14.3,0-27.4-5.2-37.5-13.9v70.3c0,20.7,16.8,37.5,37.5,37.5h0c20.7,0,37.5-16.8,37.5-37.5v-70.3 C202.4,227.4,189.3,232.7,175,232.7z"/> </g> <g> <path d="M289.1,137.5h-70.4c8.8,10.1,14.1,23.3,14.1,37.8c0,14.2-5.1,27.2-13.6,37.2h69.9c20.7,0,37.5-16.8,37.5-37.5v0 C326.6,154.3,309.9,137.5,289.1,137.5z"/> <path d="M117.5,175.3c0-14.5,5.3-27.7,14.1-37.8H60.9c-20.7,0-37.5,16.8-37.5,37.5v0c0,20.7,16.8,37.5,37.5,37.5h70.3 C122.6,202.5,117.5,189.5,117.5,175.3z"/> </g> </g> </g> </svg>`,
+        logo: `<svg viewBox="0 0 350 350" aria-hidden="true"><style>.st0{fill:#326BB3;}.st1{opacity:.49;fill:#326BB3;}.st2{opacity:.49;fill:#326BB3;stroke:#336BB3;stroke-width:5;stroke-linecap:round;stroke-miterlimit:10;}</style><g><circle class="st0" cx="175" cy="175" r="40.4"/><circle class="st1" cx="175" cy="175" r="48.7"/></g><g><circle class="st0" cx="301.3" cy="48.7" r="40.4"/><circle class="st1" cx="301.3" cy="48.7" r="48.7"/></g><g><circle class="st0" cx="48.7" cy="48.7" r="40.4"/><circle class="st1" cx="48.7" cy="48.7" r="48.7"/></g><g><circle class="st0" cx="48.7" cy="301.3" r="40.4"/><circle class="st1" cx="48.7" cy="301.3" r="48.7"/></g><g><circle class="st0" cx="301.3" cy="301.3" r="40.4"/><circle class="st1" cx="301.3" cy="301.3" r="48.7"/></g><line class="st2" x1="91.4" y1="91.4" x2="132.4" y2="132.4"/><line class="st2" x1="217.6" y1="217.6" x2="258.6" y2="258.6"/><line class="st2" x1="217.6" y1="132.4" x2="258.6" y2="91.4"/><line class="st2" x1="91.4" y1="258.6" x2="132.4" y2="217.6"/></svg>`,
         dashboard: `<svg viewBox="0 0 350 350" aria-hidden="true"><g><path d="M251.9,25c8,0,16,0,23.9,0c6,0,11.7,1,17.3,2.9c10.6,3.6,18.4,10.6,24.3,19.9c4.9,7.8,6.9,16.4,7,25.5c0.1,16.5,0.1,33,0,49.5c-0.1,13.1-4.5,24.5-13.7,34c-5,5.1-11,8.7-17.7,11.2c-5.8,2.1-11.7,2.9-17.8,2.9c-15.7,0.1-31.4,0.1-47.1,0c-7.1,0-14-1.1-20.6-4c-7.8-3.4-14.1-8.5-19.2-15.3c-3.1-4.1-5.3-8.6-7-13.4c-1.6-4.9-2.3-10.1-2.3-15.3c0-16.7,0-33.5,0-50.2c0-6.8,1.2-13.4,4-19.6c3.6-7.9,9-14.3,16-19.4c5-3.6,10.5-5.9,16.4-7.3c4.7-1.1,9.4-1.4,14.2-1.4C237.2,25,244.5,25,251.9,25z"/><path d="M251.7,179.1c8.3,0,16.5,0,24.8,0c5.7,0,11.2,1,16.6,2.9c11.6,4,19.9,11.7,25.8,22.2c2.9,5.2,4.7,10.8,5.1,16.8c0.3,3.8,0.5,7.5,0.5,11.3c0.1,14.4,0,28.7,0,43.1c0,5.8-0.6,11.5-2.4,17c-2.7,8.1-7.3,15-13.7,20.7c-4.5,4-9.6,6.8-15.2,8.9c-6,2.3-12.2,2.9-18.5,3c-15.7,0.1-31.3,0.1-47,0c-6.9,0-13.7-1.1-20.2-4c-9.1-4-16.1-10.2-21.4-18.6c-4.9-7.7-7-16.3-7-25.3c-0.1-16.8,0-33.6,0-50.3c0-6.7,1.2-13.2,3.9-19.3c4-8.9,10.2-16,18.6-21.3c8-5,16.8-7,26.2-7.1C235.8,179.1,243.7,179.1,251.7,179.1z"/><path d="M98.1,25c7.8,0,15.7,0.1,23.5,0c6.2-0.1,12.1,1,17.9,2.9c8.8,2.9,15.7,8.5,21.4,15.6c3.3,4.2,5.8,9,7.5,14.1c1.6,5.1,2.4,10.4,2.4,15.7c0.1,16.4,0.1,32.9,0,49.3c0,5.2-0.7,10.4-2.3,15.4c-3.2,9.5-8.8,17.3-16.8,23.3c-5.7,4.3-12.2,7.1-19.1,8.5c-3.3,0.7-6.8,0.9-10.2,1c-16.1,0.1-32.3,0.1-48.4,0c-13.2-0.1-24.8-4.2-34.3-13.6c-5-5-8.8-10.8-11.2-17.5c-2.2-6-2.9-12.2-2.9-18.5c0-15.9,0-31.7,0-47.6c0-5.5,0.6-10.9,2.3-16.1c2.5-7.4,6.5-13.9,12.3-19.3c4.2-4,8.8-7.4,14.2-9.5c6.1-2.4,12.4-3.8,18.9-3.8C81.5,25,89.8,25,98.1,25z"/><path d="M25.4,251.1c0-8,0-16.1,0-24.1c0-6.9,1.2-13.5,4-19.7c3.6-7.9,9-14.4,16-19.5c5-3.6,10.5-5.9,16.5-7.3c4.4-1,8.9-1.4,13.4-1.4c15.8,0,31.6,0,47.4,0c5.8,0,11.3,1,16.8,2.9c11.2,3.9,19.3,11.3,25.3,21.4c4.3,7.4,6,15.5,6.1,24c0.1,16.5,0.1,33.1,0,49.6c0,5.2-0.7,10.3-2.3,15.3c-3.2,9.5-8.8,17.4-16.9,23.4c-6.9,5-14.6,8.3-23.2,8.8c-3.7,0.2-7.3,0.5-11,0.5c-14.5,0.1-29,0.1-43.5,0c-6.9-0.1-13.6-1.2-20-4c-9-4-16.1-10.2-21.3-18.5c-5-7.9-7-16.5-7.1-25.7C25.3,268.2,25.4,259.6,25.4,251.1z"/></g></svg>`,
         calendar: `<svg viewBox="0 0 350 350" aria-hidden="true"><g><path d="M175.1,140.4c49.2,0,98.4,0,147.5,0c2.1,0,2.1,0,2.1,2.1c0,36,0,72,0,108c0,5.5-0.4,10.9-1.6,16.3c-1.3,6.1-3.3,11.9-6.1,17.5c-4.9,9.9-11.8,18.2-20.4,25.1c-6.3,5-13.3,8.7-20.9,11.4c-8.3,2.9-16.8,4.2-25.5,4.2c-50.1,0-100.3,0-150.4,0c-5.5,0-11-0.5-16.4-1.6c-6-1.2-11.8-3.3-17.4-6c-9.3-4.4-17-10.9-23.6-18.6c-4.4-5.1-7.8-10.8-10.6-16.9c-4.6-10.2-6.5-20.9-6.5-32.1c0.1-35.8,0-71.6,0-107.4c0-2,0-2,2-2C76.6,140.4,125.8,140.4,175.1,140.4z"/><path d="M323.7,117.2c-99.2,0-198.2,0-297.7,0c1.2-4.3,2.3-8.3,3.6-12.3c1.7-5.4,4.3-10.3,7.3-15c5.8-9.1,13.3-16.5,22.4-22.3c4.9-3.1,10.1-5.9,15.7-7.7c3-1,6.2-1.7,9.3-2.4c2.2-0.5,2.4-0.5,2.4-2.8c0-5.6,0-11.3,0-16.9c0-3.1,0.7-6,2.7-8.4c5.5-6.5,14.6-5.5,18.9,1.5c1.3,2.1,1.5,4.3,1.5,6.6c0,5.5,0,10.9,0,16.4c0,1.9,0.1,2,2,2c42.1,0,84.3,0,126.4,0c2,0,2.1-0.1,2.1-2.1c0-5.6,0-11.3,0-16.9c0-4.8,2.2-8.3,6.3-10.5c4.2-2.2,8.2-1.7,11.9,1c3.4,2.5,4.8,5.9,4.8,10c0,5.8,0,11.6,0,17.5c0,2.1,0.1,2.2,2.2,2.5c5.2,0.8,10.2,2.4,15,4.6c8,3.6,15.2,8.4,21.6,14.5c6.9,6.6,12.2,14.2,16.2,22.9C320.9,105,322.7,110.9,323.7,117.2z"/></g></svg>`,
         library: `<svg viewBox="0 0 350 350" aria-hidden="true"><path d="M296.8,325H53.2c-15.4,0-27.8-12.5-27.8-27.8V172.6c0-15.4,12.5-27.8,27.8-27.8h243.6c15.4,0,27.8,12.5,27.8,27.8v124.5C324.6,312.5,312.1,325,296.8,325z"/><path d="M272,115.1H76.2c-7.9,0-14.3-6.4-14.3-14.3v-1.6c0-7.9,6.4-14.3,14.3-14.3H272c7.9,0,14.3,6.4,14.3,14.3v1.6C286.3,108.7,279.9,115.1,272,115.1z"/><path d="M234.5,55.2H115.5c-7.9,0-14.3-6.4-14.3-14.3v-1.6c0-7.9,6.4-14.3,14.3-14.3h119.1c7.9,0,14.3,6.4,14.3,14.3v1.6C248.8,48.8,242.4,55.2,234.5,55.2z"/></svg>`,
@@ -114,7 +114,7 @@
       <div class="gp-sidebar__panel">
         <div class="gp-sidebar__top">
           <div class="gp-brand">
-            <div class="gp-brand__mark gp-brand__mark--logo">${iconSvg("logo")}</div>
+            <div class="gp-brand__mark gp-brand__mark--logo"><img src="assets/portal-logo-new.svg" alt="Government Collaboration Portal" class="gp-brand__logo-img"></div>
             <div class="gp-brand__text">
               <div class="gp-brand__title"><span class="gp-brand__title-main">Government Collaboration</span><span class="gp-brand__title-sub">Portal</span></div>
             </div>
@@ -144,36 +144,33 @@
     `;
 
     const body = document.body;
-    const isCompactViewport = () => window.matchMedia('(max-width: 980px)').matches;
-    const openMenu = () => {
-      body.classList.add('gp-menu-open');
-      body.classList.remove('gp-sidebar-expanded');
-    };
+    const isDesktopSidebar = () => window.innerWidth > 980;
+    const openMenu = () => body.classList.add('gp-menu-open');
     const closeMenu = () => body.classList.remove('gp-menu-open');
-    const expandSidebar = () => {
-      if (isCompactViewport()) return;
-      body.classList.add('gp-sidebar-expanded');
-    };
-    const collapseSidebar = () => {
-      if (isCompactViewport()) return;
-      body.classList.remove('gp-sidebar-expanded');
-    };
+    const expandSidebar = () => { if (isDesktopSidebar()) body.classList.add('gp-sidebar-expanded'); };
+    const collapseSidebar = () => body.classList.remove('gp-sidebar-expanded');
 
-    sidebar.addEventListener('mouseenter', expandSidebar);
-    sidebar.addEventListener('mouseleave', collapseSidebar);
-    sidebar.addEventListener('focusin', expandSidebar);
+    sidebar.addEventListener('mouseenter', () => { if (isDesktopSidebar()) expandSidebar(); });
+    sidebar.addEventListener('mouseleave', () => { if (isDesktopSidebar()) collapseSidebar(); });
+    sidebar.addEventListener('focusin', () => { if (isDesktopSidebar()) expandSidebar(); });
     sidebar.addEventListener('focusout', (event) => {
-      if (!sidebar.contains(event.relatedTarget)) collapseSidebar();
+      if (isDesktopSidebar() && !sidebar.contains(event.relatedTarget)) collapseSidebar();
     });
 
-    sidebar.querySelector('.gp-mobile-toggle')?.addEventListener('click', (e) => { e.preventDefault(); e.stopPropagation(); openMenu(); });
-    sidebar.querySelector('.gp-mobile-close')?.addEventListener('click', (e) => { e.preventDefault(); e.stopPropagation(); closeMenu(); });
+    sidebar.querySelector('.gp-mobile-toggle')?.addEventListener('click', openMenu);
+    sidebar.querySelector('.gp-mobile-close')?.addEventListener('click', closeMenu);
     sidebar.querySelector('.gp-sidebar__scrim')?.addEventListener('click', closeMenu);
     sidebar.querySelectorAll('.gp-nav__link').forEach((link) => link.addEventListener('click', closeMenu));
-    window.addEventListener('keydown', (e) => { if (e.key === 'Escape') closeMenu(); });
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') closeMenu();
+    });
     window.addEventListener('resize', () => {
-      if (!isCompactViewport()) closeMenu();
-      if (isCompactViewport()) body.classList.remove('gp-sidebar-expanded');
+      if (window.innerWidth > 980) {
+        closeMenu();
+      } else {
+        collapseSidebar();
+      }
     });
 
     sidebar.querySelector('#logoutBtn').addEventListener('click', () => {
